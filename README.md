5c515d3ae3acf929ebcdfacca4a72b9a5e0cb376
jmrf <jose.marcos.rf@gmail.com>
2017-03-14 00:18:10 +0100

    Generalized the cart-pole script to be usable for other envs

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ matplotlib==2.0.0
 gym==0.8.0
 
 
+How To
+------
+
+To train a dueling network on the cart-pole environment and render when the average reward is above 50:
+```sh
+python q_learn.py CartPole-v0 --render 50
+```
+
+
--- q_learn.py
+++ q_learn.py
@@ -2,12 +2,14 @@ import os
 import gym
 import math
 import keras
+import pprint
 import logging
 import argparse
 import numpy as np
 import keras.backend as K
 import matplotlib.pyplot as plt
 
+from gym import envs
 from gym import wrappers
 from tendo import colorer
 from keras.models import Model
@@ -24,36 +26,28 @@ logger.setLevel(logging.DEBUG)
 # TODO: measure impact of replay factor resetting
 
 
-# We aim to solve the balancing pole
-env = gym.make('CartPole-v0')
-env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1', force=True)
-
-# Environment parameters
-A = env.action_space.n
-D = env.observation_space.shape
-
-def get_model(hidden_size):
+def get_model(hidden_size, num_actions, space_shape):
     """ Defines an MLP Q-network takes an observation from the environment
         and outputs a Q value per action.
         In this case there are two actions, i.e: force applyied to the cart (-1, +1)
     """
-    inp = Input(shape=D)
+    inp = Input(shape=space_shape)
     x = Dense(hidden_size, activation="relu")(inp)
     # x = Dropout(p=0.25)(x)
-    q = Dense(A, activation="linear")(x)
+    q = Dense(num_actions, activation="linear")(x)
     model = Model(input=inp, output=q)
     print(model.summary())
     return model
 
 
-def get_dueling_model(hidden_size, mode="mean"):
+def get_dueling_model(hidden_size, num_actions, space_shape, mode):
     """ Defines a dueling Q-network with three different aggregation modes: {avg, max, naive}
         Takes an observation from the environment and outputs a Q value per action
     """
-    inp = Input(shape=D)
+    inp = Input(shape=space_shape)
     h = Dense(hidden_size, activation="relu")(inp)
     # h = Dropout(p=0.25)(h)
-    y = Dense(A + 1)(h)
+    y = Dense(num_actions + 1)(h)
     if mode == 'avg':
         z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.mean(a[:, 1:], keepdims=True),
                    output_shape=(A,))(y)
@@ -61,7 +55,7 @@ def get_dueling_model(hidden_size, mode="mean"):
         z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.max(a[:, 1:], keepdims=True),
                    output_shape=(A,))(y)
     elif mode == 'naive':
-        z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:], output_shape=(env.action_space.n,))(y)
+        z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:], output_shape=(num_actions,))(y)
     else:
         raise ValueError("Invalid mode: {} ".format(mode))
     model = Model(input=inp, output=z)
@@ -72,6 +66,7 @@ def get_dueling_model(hidden_size, mode="mean"):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('env', default='CartPole-v0', help="GYM Environment")
     parser.add_argument('--batch_size', type=int, default=100)
     parser.add_argument('--hidden_size', type=int, default=150)
     parser.add_argument('--replay_size', type=int, default=100)
@@ -79,46 +74,60 @@ if __name__ == "__main__":
     parser.add_argument('--gamma', type=float, default=0.99, help="reward discount factor")
     parser.add_argument('--lr', type=float, default=1e-3, help="learning rate")
     parser.add_argument('--epsilon', type=float, default=0.1, help="Exploration probability")
+    parser.add_argument('--exploration_decay', type=float, default=1e-5)
     parser.add_argument('--max_episodes', type=int, default=200)
     parser.add_argument('--nn_mode', default="max", help="aggregation mode for dueling network")
-    parser.add_argument('--model_save_path', default="cart-pole-mlp")
+    parser.add_argument('--model_path', default="cart-pole-mlp")
     parser.add_argument('--non_dueling', action="store_true", help="whether to use a non-dueling network")
+    parser.add_argument('--render', type=float, default=100, help="minimum avg reward to start rendering")
     args = parser.parse_args()
+
+    avail_envs = [e.id for e in envs.registry.all()]
+    if args.env not in avail_envs:
+        logger.error("{} is not a valid env".format(args.env))
+        raise ValueError("env must be one of: {}".format(pprint.pformat(avail_envs)))
+
+    # We aim to solve the balancing pole
+    env = gym.make(args.env)
+    env_name = args.env.split("-")[0]
+    env = wrappers.Monitor(env, '/tmp/{}-experiment-1'.format(env_name), force=True)
+
+    # Environment parameters
+    A = env.action_space.n
+    D = env.observation_space.shape
 
     if args.batch_size > args.replay_size:
         logger.warning("Replay size should be bigger than replay size to guarantee batches of the given size")
 
     # define and maybe load model
     if args.non_dueling:
-        model = get_dueling_model(args.hidden_size)
+        model = get_model(args.hidden_size,A, D)
     else:
-        model = get_dueling_model(args.hidden_size, mode=args.nn_mode)
+        model = get_dueling_model(args.hidden_size,A, D, mode=args.nn_mode)
 
-    if os.path.exists(args.model_save_path):
-        logger.info("Loading model from: {}".format(args.model_save_path))
-        model.load_weights(args.model_save_path)
+    # load model if exists
+    if os.path.exists(args.model_path):
+        logger.info("Loading model from: {}".format(args.model_path))
+        model.load_weights(args.model_path)
     optim = Adam(lr=args.lr, decay=1e-5)
     model.compile(optimizer=optim, loss='mse')
 
     pre_states, actions, rewards, post_states, terminal = [], [], [], [], []   # per episode data
+    timesteps = 0
     total_reward = 0
+    avg_reward = -float("inf")
     episode_number = 1
     episode_reward = 0
-    timesteps = 0
     rendering = False
+    exploration_factor = args.epsilon
 
     observation = env.reset()   # Obtain an initial observation of the environment
-        
     while episode_number <= args.max_episodes:
-        
-        # Rendering the environment slows things down, 
-        # so let's only look at it once our model is doing a good job.
-        if total_reward/episode_number > 50 or rendering == True: 
+        if avg_reward > args.render: 
             env.render()
-            rendering = True
         
-        # greedy versus exploration (policy)
-        if np.random.uniform() < args.epsilon:   # change this to explore more at the begining
+        # epsilon-greedy policy
+        if np.random.uniform() < exploration_factor:
             action = np.random.randint(A)
         else:
             s = np.array([observation])
@@ -143,7 +152,8 @@ if __name__ == "__main__":
             for k in range(args.train_repeat):
                 # sample from experience buffer
                 sample_idx = np.random.choice(len(pre_states), size=args.batch_size)
-                # get Q values for all the sample states s --> s'
+                # get Q values for all the sample states s --> s' (we use a TD(0))
+                # (sample backup as we only take one action into account for the update)
                 q_pre_states = model.predict(np.array(pre_states)[sample_idx])      # q_t(s,a)
                 q_post_states = model.predict(np.array(post_states)[sample_idx])    # q_t+1(s,a)
                 # compute deltas
@@ -167,6 +177,8 @@ if __name__ == "__main__":
             timesteps = 0
             episode_reward = 0
             episode_number += 1
+            exploration_factor /= (1.0 + args.exploration_decay)
+
             # reset the replay buffer ==> easy with mem 
             max_buf_size = args.replay_size * 5
             if len(pre_states) > max_buf_size:
@@ -181,5 +193,5 @@ if __name__ == "__main__":
 logger.info("Average reward per episode {}".format(total_reward / args.max_episodes))
 logger.info('{} Episodes completed.'.format(args.max_episodes))
 
-logger.info("saving model as {}".format(args.model_save_path))
-model.save(args.model_save_path)
+logger.info("saving model as {}".format(args.model_path))
+model.save(args.model_path)
